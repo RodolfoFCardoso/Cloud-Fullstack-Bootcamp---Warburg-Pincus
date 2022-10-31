1f6b35ea39807d984836fdd71f73899686b3252b
RodolfoFCardoso <rodolfo.fredd@gmail.com>
2022-10-31 10:25:20 -0300

    Editado arquivo exercicio02.js da pasta funções

--- 04 - Função/exercicio02.js
+++ 04 - Função/exercicio02.js
@@ -1,16 +1,21 @@
 //Realizar o calculo para diferentes formas de pagamentos
 
-// Legenda condPgto 1- A vista, 2- Pix ou Dinheiro, 4- 2x, 5- Mais de 2x
-let valorProduto = 50.00, condPgto = 0;
+// Legenda condPgto 1- A vista, 2- Pix ou Dinheiro, 3- 2x, 4- Mais de 2x
 
+let valorProduto = 100.00;
 
-
-if (condPgto === 1) {
-   console.log(valorProduto = valorProduto - (valorProduto * 0.10))
-} else if (condPgto === 2) {
-    console.log(valorProduto = valorProduto - (valorProduto * 0.15))
-} else if (condPgto === 3) {
-    console.log(valorProduto)
-} else {
-    console.log(valorProduto = valorProduto + (valorProduto * 0.10))   
+function condPgto(condPgto) {
+    if (condPgto === 1) {
+        return valorProduto = valorProduto - (valorProduto * 0.10)
+    } else if (condPgto === 2) {
+        return valorProduto = valorProduto - (valorProduto * 0.15)
+    } else if (condPgto === 3) {
+        return valorProduto
+    } else {
+        return valorProduto = valorProduto + (valorProduto * 0.10)
+    }
 }
+
+condPgto(1);
+
+console.log('O valor do produto a ser pago é '+ valorProduto);

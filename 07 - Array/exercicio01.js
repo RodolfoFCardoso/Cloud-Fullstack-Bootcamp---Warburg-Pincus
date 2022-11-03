9cd74d139443748e012a83299139d8c0ed51bb71
RodolfoFCardoso <rodolfo.fredd@gmail.com>
2022-11-03 13:37:49 -0300

    Adição dos exercicios práticos de Array

--- 07 - Array/exercicio01.js
+++ 07 - Array/exercicio01.js
@@ -1,8 +1,8 @@
 //crie um programa que dado o número, imprima sua tabuada.
 
-let tabuada = [0,1,2,3,4,5,6,7,8,9,10];
+const tabuada = [0,1,2,3,4,5,6,7,8,9,10];
 
-numero = 10;
+const numero = 8;
 
 for (let i = 0; i < tabuada.length; i++) {
     let resultadoTabuada = tabuada[i]*numero;

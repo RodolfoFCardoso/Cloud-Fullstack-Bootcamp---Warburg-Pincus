09b56e0a968dce2fef6ba99d1cd26fb08fba1129
RodolfoFCardoso <rodolfo.fredd@gmail.com>
2022-11-04 11:17:47 -0300

    Adição das aulas de Importação

---
+++ 08 - Importação/exercicio01.js
@@ -0,0 +1,15 @@
+const {gets, print} = require('./funcoes-auxiliares');
+
+let maiorValor = 0;
+
+for (let i = 0; i < 5; i++) {
+    const numeroSorteado = gets();
+    if (maiorValor < numeroSorteado) {
+        maiorValor = numeroSorteado;
+    }   
+}
+
+
+
+
+print(maiorValor);
